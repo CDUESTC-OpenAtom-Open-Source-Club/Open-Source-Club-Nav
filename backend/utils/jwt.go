ea0e212bc442926200bfa349ba2ef2cdebcaf7d4
Nerdlet369 <chenchanhming2025@qq.com>
2026-05-16 17:39:21 +0800

    Add GenerateToken function for JWT
    Implement JWT token generation with expiration.

---
+++ backend/utils/jwt.go
@@ -0,0 +1,20 @@
+// utils/jwt.go
+package utils
+
+import (
+	"Open-Source-Club-Nav/backend/config"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// 生成JWT Token
+func GenerateToken(username string) (string, error) {
+	cfg := config.LoadConfig()
+	claims := jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Second * time.Duration(cfg.JWT.Expire)).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(cfg.JWT.Secret))
+}
