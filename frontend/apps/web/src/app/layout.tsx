f5c923223bbff9cd3b29143614ba324145daee8e
muzimu217 <1278844978@qq.com>
2026-06-20 02:28:58 -0700

    chore: 开启51.la录屏功能(screenRecord)

--- frontend/apps/web/src/app/layout.tsx
+++ frontend/apps/web/src/app/layout.tsx
@@ -155,7 +155,7 @@ export default function RootLayout({
           strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html:
-              'LA.init({id:"3QJPple08RRBVP8s",ck:"3QJPple08RRBVP8s",autoTrack:true,hashMode:true})',
+              'LA.init({id:"3QJPple08RRBVP8s",ck:"3QJPple08RRBVP8s",autoTrack:true,hashMode:true,screenRecord:true})',
           }}
         />
       </head>
